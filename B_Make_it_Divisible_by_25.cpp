2da5b4c2e7b8c95468420d264147e4fe367dde23
notoveryet-51 <sray387.official@gmail.com>
2026-03-14 17:35:29 +0000

    Added codeforces 1593B solution

---
+++ B_Make_it_Divisible_by_25.cpp
@@ -0,0 +1,154 @@
+/*The program is the solution of codeforces problem no. 1593B
+Regn no.: 2025CA085
+Program Date: 14-03-2026    */
+
+/**
+ * ███████╗ █████╗ ███╗   ██╗██████╗ ██╗██████╗  █████╗ ███╗   ██╗
+ * ██╔════╝██╔══██╗████╗  ██║██╔══██╗██║██╔══██╗██╔══██╗████╗  ██║
+ * ███████╗███████║██╔██╗ ██║██║  ██║██║██████╔╝███████║██╔██╗ ██║
+ * ╚════██║██╔══██║██║╚██╗██║██║  ██║██║██╔═══╝ ██╔══██║██║╚██╗██║
+ * ███████║██║  ██║██║ ╚████║██████╔╝██║██║     ██║  ██║██║ ╚████║
+ * ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝
+ *
+ * S A N D I P A N   R A Y
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sza(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define gcd(a, b) __gcd(a, b)
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vc = vector<char>;
+using umii = unordered_map<int, int>;
+using umci = unordered_map<char, int>;
+using umsi = unordered_map<string, int>;
+using si = set<int>;
+using sc = set<char>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return (a % m * b % m) % m; }
+
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+ll binpow(ll b, ll n) {
+    ll result = 1;
+    while (n > 0) {
+        if (n & 1) result *= b;
+        b *= b;
+        n >>= 1;
+    }
+    return result;
+}
+
+bool prime(ll a) {
+    static const ll MAX = 1000005;
+    static vector<bool> isPrime(MAX, true);
+    static bool initialized = false;
+    if (!initialized) {
+        isPrime[0] = isPrime[1] = false;
+        for (ll i = 2; i * i < MAX; ++i) {
+            if (isPrime[i]) {
+                for (ll j = i * i; j < MAX; j += i)
+                    isPrime[j] = false;
+            }
+        }
+        initialized = true;
+    }
+    if (a < 0 || a >= MAX) return false;
+    return isPrime[a];
+}
+
+inline void yes() { cout << "YES\n"; }
+inline void no()  { cout << "NO\n"; }
+
+template <typename T>
+void read(vector<T>& v) { for (auto& x : v) cin >> x; }
+
+template <typename T>
+void print(const vector<T>& v) { for (const auto& x : v) cout << x << ' '; cout << '\n'; }
+
+void solve() {
+    string s;   cin>>s;
+    ll n=s.size();
+    if (s[n-1]=='0' && s[n-2]=='0' || s[n-1]=='5' && s[n-2]=='2' || s[n-1]=='5' && s[n-2]=='7' || s[n-1]=='0' && s[n-2]=='5')   {
+        cout<<0<<endl;  return;
+    }
+    ll ans = n; 
+    for (char target : {'0', '5'}) { 
+        ll idx_prev=-1,idx_nxt=-1;
+        ll count_rem=0;
+        for (ll i=0; i<n; i++)  {
+            if(s[i]==target)    idx_nxt=i;
+        }
+        if (idx_nxt == -1) continue;
+        if (idx_nxt!=n-1)   count_rem=n-idx_nxt-1;
+        if (s[idx_nxt]=='0') {
+            for (ll i=idx_nxt-1;i>=0;i--) {
+                if (s[i]=='0' || s[i]=='5') {
+                    idx_prev=i;
+                    break;
+                }
+            }
+            if (idx_prev != -1) { 
+                count_rem+=(idx_nxt-idx_prev-1);
+                ans = min(ans, count_rem); 
+            }
+        }
+        else {
+            for (ll i=idx_nxt-1;i>=0;i--) {
+                if (s[i]=='2' || s[i]=='7') {
+                    idx_prev=i;
+                    break;
+                }
+            }
+            if (idx_prev != -1) { 
+                count_rem+=(idx_nxt-idx_prev-1);
+                ans = min(ans, count_rem); 
+            }
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
