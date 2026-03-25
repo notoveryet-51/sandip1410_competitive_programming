0b1e4c7e67cca44b8b3a6beb4544e7d0f732768f
notoveryet-51 <sray387.official@gmail.com>
2026-03-25 21:02:35 +0530

    Added codechef start231 solutions

---
+++ Imperfect_Numbers.cpp
@@ -0,0 +1,125 @@
+/*The program is the solution of codechef problem start231b
+Regn no.: 2025CA085
+Program Date: 25-03-2026    */
+
+/**
+ *  ███████╗ █████╗ ███╗   ██╗██████╗ ██╗██████╗  █████╗ ███╗   ██╗
+ *  ██╔════╝██╔══██╗████╗  ██║██╔══██╗██║██╔══██╗██╔══██╗████╗  ██║
+ *  ███████╗███████║██╔██╗ ██║██║  ██║██║██████╔╝███████║██╔██╗ ██║
+ *  ╚════██║██╔══██║██║╚██╗██║██║  ██║██║██╔═══╝ ██╔══██║██║╚██╗██║
+ *  ███████║██║  ██║██║ ╚████║██████╔╝██║██║     ██║  ██║██║ ╚████║
+ *  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝
+ *
+ *                 S A N D I P A N   R A Y
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sza(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define gcd(a, b) __gcd(a, b)
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vc = vector<char>;
+using umii = unordered_map<int, int>;
+using umci = unordered_map<char, int>;
+using umsi = unordered_map<string, int>;
+using si = set<int>;
+using sc = set<char>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return (a % m * b % m) % m; }
+
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+ll binpow(ll b, ll n) {
+    ll result = 1;
+    while (n > 0) {
+        if (n & 1) result *= b;
+        b *= b;
+        n >>= 1;
+    }
+    return result;
+}
+
+bool prime(ll a) {
+    static const ll MAX = 1000005;
+    static vector<bool> isPrime(MAX, true);
+    static bool initialized = false;
+    if (!initialized) {
+        isPrime[0] = isPrime[1] = false;
+        for (ll i = 2; i * i < MAX; ++i) {
+            if (isPrime[i]) {
+                for (ll j = i * i; j < MAX; j += i)
+                    isPrime[j] = false;
+            }
+        }
+        initialized = true;
+    }
+    if (a < 0 || a >= MAX) return false;
+    return isPrime[a];
+}
+
+inline void yes() { cout << "YES\n"; }
+inline void no()  { cout << "NO\n"; }
+
+template <typename T>
+void read(vector<T>& v) { for (auto& x : v) cin >> x; }
+
+template <typename T>
+void print(const vector<T>& v) { for (const auto& x : v) cout << x << ' '; cout << '\n'; }
+
+void solve() {
+    ll n;   cin>>n;
+    if ((n%5==0 || n%2==0) && n%10!=0)    {
+        cout<<0<<endl;
+        return;
+    }
+    else {
+        ll mini=min(n%2,n%5);
+        if (mini==0)    cout<<2<<endl;
+        else cout<<mini<<endl;
+        return;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
