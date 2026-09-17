8b35f36ddbce759f3abbfac73b4ab46392b1fc40
notoveryet-51 <sray387.official@gmail.com>
2026-09-17 20:32:34 +0530

    Added codeforces 456B solution

---
+++ B_Fedya_and_Maths.cpp
@@ -0,0 +1,141 @@
+/*The program is the solution of codeforces problem no. 456B
+Regn no.: 2025CA085
+Program Date: 17-09-2026
+Program Time: 19:37 IST    */
+
+/**
+ *  ███████╗ █████╗ ███╗   ██╗██████╗ ██╗██████╗  █████╗ ███╗   ██╗
+ *  ██╔════╝██╔══██╗████╗  ██║██╔══██╗██║██╔══██╗██╔══██╗████╗  ██║
+ *  ███████╗███████║██╔██╗ ██║██║  ██║██║██████╔╝███████║██╔██╗ ██║
+ *  ╚════██║██╔══██║██║╚██╗██║██║  ██║██║██╔═══╝ ██╔══██║██║╚██╗██║
+ *  ███████║██║  ██║██║ ╚████║██████╔╝██║██║     ██║  ██║██║ ╚████║
+ *  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝
+ *
+ *                 S A N D I P A N   R A Y
+ */
+
+/*----------------------------------INCLUDES & NAMESPACE----------------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/*----------------------------------MACROS----------------------------------*/
+#define sza(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define gcd(a, b) __gcd(a, b)
+
+/*----------------------------------TYPE ALIASES----------------------------------*/
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vc = vector<char>;
+using umii = unordered_map<int, int>;
+using umci = unordered_map<char, int>;
+using umsi = unordered_map<string, int>;
+using si = set<int>;
+using sc = set<char>;
+
+/*----------------------------------CONSTANTS----------------------------------*/
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+/*----------------------------------MATH FUNCTIONS----------------------------------*/
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return (a % m * b % m) % m; }
+
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+/*----------------------------------BINARY EXPONENTIATION----------------------------------*/
+ll binpow(ll b, ll n) {
+    ll result = 1;
+    while (n > 0) {
+        if (n & 1) result *= b;
+        b *= b;
+        n >>= 1;
+    }
+    return result;
+}
+
+/*----------------------------------PRIME SIEVE FUNCTION----------------------------------*/
+bool prime(ll a) {
+    static const ll MAX = 1000005;
+    static vector<bool> isPrime(MAX, true);
+    static bool initialized = false;
+    if (!initialized) {
+        isPrime[0] = isPrime[1] = false;
+        for (ll i = 2; i * i < MAX; ++i) {
+            if (isPrime[i]) {
+                for (ll j = i * i; j < MAX; j += i)
+                    isPrime[j] = false;
+            }
+        }
+        initialized = true;
+    }
+    if (a < 0 || a >= MAX) return false;
+    return isPrime[a];
+}
+
+/*----------------------------------UTILITY OUTPUT FUNCTIONS----------------------------------*/
+inline void yes() { cout << "YES\n"; }
+inline void no()  { cout << "NO\n"; }
+
+/*----------------------------------INPUT/OUTPUT HELPERS----------------------------------*/
+template <typename T>
+void read(vector<T>& v) { for (auto& x : v) cin >> x; }
+
+template <typename T>
+void print(const vector<T>& v) { for (const auto& x : v) cout << x << ' '; cout << '\n'; }
+
+/*----------------------------------SOLVE FUNCTION----------------------------------*/
+void solve() {
+    string s;   cin>>s;
+    ll sum;
+    if (s.size()<2) {
+        ll temp=stoi(s);
+        cout<<(ll)(pow(1, temp)+pow(2, temp)+pow(3, temp)+pow(4, temp))%5<<endl;
+        return;
+    }
+    ll n=s.size();
+    ll digits=stoi(s.substr(n-2));
+    ll rem=digits%4;
+    if (rem==1) sum=1+2+3+4;
+    else if (rem==2) sum=1+4+9+6;
+    else if (rem==3) sum=1+8+7+4;
+    else sum=1+6+1+6;
+    cout<<sum%5<<endl;
+}
+
+/*----------------------------------MAIN FUNCTION----------------------------------*/
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    // int t;
+    // cin >> t;
+    // while (t--) {
+    //     solve();
+    // }
+    return 0;
+}
